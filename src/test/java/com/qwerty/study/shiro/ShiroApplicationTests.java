4bd42cff84eb24791d215feea45185635b0fd011
zhou <2891128968@qq.com>
2019-06-04 20:17:22 +0800

    2019.5.21 21:35 springboot中的shiro使用

--- src/test/java/com/qwerty/study/shiro/ShiroApplicationTests.java
+++ src/test/java/com/qwerty/study/shiro/ShiroApplicationTests.java
@@ -1,7 +1,9 @@
 package com.qwerty.study.shiro;
 
+import com.qwerty.study.shiro.realm.MyShiroRealm;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ShiroApplicationTests {
 
+	@Autowired
+	private MyShiroRealm myShiroRealm;
+
 	@Test
 	public void contextLoads() {
+//		System.out.println(new MyShiroRealm().getUserInfoService());
 	}
 
 }
